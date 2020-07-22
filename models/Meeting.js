5f7ce7d96321eb40a36f3a1e496c4daedcb62bbf
luis franco galvan <galvan7725@gmail.com>
2020-07-22 12:25:25 -0500

    Meeting model

---
+++ models/Meeting.js
@@ -0,0 +1,24 @@
+'use strict'
+const mongoose = require('mongoose');
+const {ObjectId}  = mongoose.Schema;
+
+const meetingSchema = new mongoose.Schema({
+    user:{
+        type: ObjectId,
+        ref: 'User'
+    },
+    dateM:{
+        type:Date,
+        required: true
+    },
+    status:{
+        type: String,
+        default:"pending"
+    },
+    comments:{
+        type: String
+    }
+    
+});
+
+module.exports = mongoose.model("Meeting",meetingSchema);
