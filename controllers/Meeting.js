15cda9b18aead9e094b72e7fb74de71ba8ce9622
luis franco galvan <galvan7725@gmail.com>
2020-08-20 19:27:17 -0500

    meeting by day

--- controllers/Meeting.js
+++ controllers/Meeting.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const Meeting = require('../models/Meeting');
 
 exports.allMeetings = (req, res) =>{
@@ -14,6 +15,41 @@ exports.allMeetings = (req, res) =>{
         }
     })
 };
+
+exports.meetingsByDate = (req, res, next,date)=>{
+    if(date.trim() !== "" && date !== undefined){
+        req.date = date;
+        next();
+    }else{
+        res.status(400).send({ error: "Invalid date" });
+    }
+}
+
+exports.getMeetingsByDate = (req, res)=>{
+    Meeting.find({$and : [{dateM:{ $gte : moment(req.date).startOf('day').toDate()}}, {dateM:{$lte : moment(req.date).endOf('isoWeek').toDate()}}]})
+    .exec((err, result)=>{
+        if (err || !result){
+            console.log(err);
+        }else{
+            console.log(result);
+            res.status(200).send(result);
+        }
+    })
+}
+
+const allM =(req, res)=>{
+    Meeting.find()
+   // .sort({dateM: 'asc'})
+    .exec((err, meetings)=>{
+        if(err || meetings === undefined){
+            res.status(400).send({
+                error:"Error"
+            })
+        }else{
+            res.status(200).send(meetings);
+        }
+    })
+} 
 
 exports.newMeeting = async (req, res)=>{
     const data={};
@@ -42,3 +78,91 @@ exports.newMeeting = async (req, res)=>{
     }
 
 }
+
+exports.meetingById = (req, res, next,meetingId) => {
+    Meeting.findById(meetingId)
+    .select("_id user._id dateM status persons description comments place updated")
+    .exec((err, result)=>{
+        if (err|| !result){
+            return res.status(400).json({
+                error: err
+            })
+        }else{
+            req.meeting = result;
+            next();
+        }
+
+    })
+
+}
+
+exports.meetingUpdateTopic = function(req, res, next,topic) {
+    if(topic.trim() !== ""){
+        req.topicM = topic;
+        next();
+    }else{
+        res.status(400).json({
+            error: "No se reconoce la accion"
+        })
+    }
+}
+const meetingSetDone =(req, res)=>{
+    Meeting.findByIdAndUpdate(req.meeting._id,{$set:{status: "done",updated:Date.now()}},{new: true})
+    .exec((err, result)=>{
+        if(err || !result){
+            res.status(400).send({
+                error: err
+            })
+        }else{
+            allM(req, res);
+        }
+    })
+}
+const meetingSetPending =(req, res)=>{
+    Meeting.findByIdAndUpdate(req.meeting._id,{$set:{status: "pending",updated:Date.now()}},{new: true})
+    .exec((err, result)=>{
+        if(err || !result){
+            res.status(400).send({
+                error: err
+            })
+        }else{
+            allM(req,res);
+        }
+    })
+}
+
+const meetingSetCancel =(req, res)=>{
+    Meeting.findByIdAndUpdate(req.meeting._id,{$set:{status: "cancel",updated:Date.now()}},{new: true})
+    .exec((err, result)=>{
+        if(err || !result){
+            res.status(400).send({
+                error: err
+            })
+        }else{
+            allM(req,res);
+        }
+    })
+}
+
+
+exports.meetingUpdateStatus =(req, res)=>{
+    console.log(req.topicM);
+    switch (req.topicM) {
+        case "done":
+            meetingSetDone(req,res);
+            break;
+        case "pending":
+            meetingSetPending(req,res);
+            break;
+        case "cancel":
+            meetingSetCancel(req,res);    
+            break;
+        default:
+            res.status(400).send({
+                error: "Accion invalida"
+            })
+            break;
+    }
+}
+
+
--- routes/meeting.js
+++ routes/meeting.js
@@ -2,16 +2,20 @@
 const express = require('express');
 const authController = require('../controllers/auth');
 const userController = require('../controllers/user');
-const { allMeetings, newMeeting } = require('../controllers/Meeting');
+const { allMeetings, newMeeting, meetingUpdateStatus,meetingById ,meetingUpdateTopic, meetingsByDate,getMeetingsByDate} = require('../controllers/Meeting');
 //const validator = require('../validator')
 
 const router = express.Router();
 
 router.get("/meetings/all",authController.requireSingin,allMeetings);
 router.post("/meetings/new",authController.requireSingin,newMeeting);
-
+router.put("/meeting/update/:topicM/:meetingId",authController.requireSingin,meetingUpdateStatus);
+router.get("/meetings/date/:meetingsDate",authController.requireSingin,getMeetingsByDate);
 
 router.param("userId", userController.userById);
 router.param("adminId", userController.adminById);
+router.param("meetingId", meetingById);
+router.param("topicM", meetingUpdateTopic);
+router.param("meetingsDate",meetingsByDate);
 
 module.exports = router;
